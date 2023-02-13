050d3771eb814a61d511a3d6db7a31a07b8dfe6a
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-02-13 15:47:28 +0200

    contactSchema

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const createError = require("http-errors");
-const Joi = require("joi");
+const {contactSchema} = require("../../contactSchema")
+// const Joi = require("joi");
 
-const contactSchema = Joi.object({
-  name: Joi.string().alphanum().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
-});
+// const contactSchema = Joi.object({
+//   name: Joi.string().alphanum().min(3).max(30).required(),
+//   email: Joi.string().email().required(),
+//   phone: Joi.string().required(),
+// });
 
 const router = express.Router();
 
